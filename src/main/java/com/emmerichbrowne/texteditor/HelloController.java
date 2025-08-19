09149152dca6431a2edaa62efe5f7c6634e8b7f4
emmerichdev <emmerichhbrowne@gmail.com>
2025-08-18 19:22:22 -0500

    Feat: new & exit, placeholders for open, save, save as.

--- src/main/java/com/emmerichbrowne/texteditor/HelloController.java
+++ src/main/java/com/emmerichbrowne/texteditor/HelloController.java
@@ -1,7 +1,31 @@
 package com.emmerichbrowne.texteditor;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
+
+    @FXML
+    private TextArea textArea;
+
+    public void onNew() {
+        textArea.clear();
+    }
+
+    public void onOpen() {
+        System.out.println("Open");
+    }
+
+    public void onSave() {
+        System.out.println("Save");
+    }
+
+    public void onSaveAs() {
+        System.out.println("Save As");
+    }
+
+    public void onExit() {
+        Platform.exit();
+    }
 }
